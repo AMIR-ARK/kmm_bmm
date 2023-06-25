6f3bc5a51326dd9f4b5ece4b86d335ebeff86984
AMIR-ARK <126375003+AMIR-ARK@users.noreply.github.com>
2023-06-25 08:52:06 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter the first number: ");
+        int num1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter the second number: ");
+        int num2 = int.Parse(Console.ReadLine());
+
+        int kmm = CalculateKMM(num1, num2);
+        int bmm = CalculateBMM(num1, num2);
+
+        Console.WriteLine("The KMM of {0} and {1} is: {2}", num1, num2, kmm);
+        Console.WriteLine("The BMM of {0} and {1} is: {2}", num1, num2, bmm);
+
+        Console.ReadKey();
+    }
+
+    static int CalculateKMM(int num1, int num2)
+    {
+        int max = Math.Max(num1, num2);
+        int min = Math.Min(num1, num2);
+
+        int kmm = max;
+        while (kmm % min != 0)
+        {
+            kmm += max;
+        }
+
+        return kmm;
+    }
+
+    static int CalculateBMM(int num1, int num2)
+    {
+        int max = Math.Max(num1, num2);
+        int min = Math.Min(num1, num2);
+
+        int bmm = min;
+        while (max % bmm != 0)
+        {
+            bmm--;
+        }
+
+        return bmm;
+    }
+}
